44e45d094fdffa7c3bcf7d56f34c2e591b352032
Mari Montoya <kulafumon@gmail.com>
2025-04-28 20:45:46 -0400

    Reinstalled Spring boot.

--- back/src/main/java/com/njwmerv/salita_back/SalitaBackApplication.java
+++ back/src/main/java/com/njwmerv/salita_back/SalitaBackApplication.java
@@ -1,13 +1,13 @@
-package com.example.back;
+package com.njwmerv.salita_back;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BackApplication {
+public class SalitaBackApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BackApplication.class, args);
+		SpringApplication.run(SalitaBackApplication.class, args);
 	}
 
 }
--- back/src/test/java/com/njwmerv/salita_back/SalitaBackApplicationTests.java
+++ back/src/test/java/com/njwmerv/salita_back/SalitaBackApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.back;
+package com.njwmerv.salita_back;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BackApplicationTests {
+class SalitaBackApplicationTests {
 
 	@Test
 	void contextLoads() {
